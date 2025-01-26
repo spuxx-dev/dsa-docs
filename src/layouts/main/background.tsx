655d2d485cf69ffa1dd2e587565dbe7119a442fa
spuxx1701 <accounts@spuxx.dev>
2025-01-26 15:38:57 +0100

    fix: Fixed layout

---
+++ src/layouts/main/background.tsx
@@ -0,0 +1,6 @@
+import type { Component } from 'solid-js';
+import styles from './background.module.css';
+
+export const Background: Component = () => {
+  return <span class={styles.background}></span>;
+};
